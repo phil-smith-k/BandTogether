c3a3c4cbdc2004113cdc262a21ffc4e66c17f96f
Philip Smith <philip.smith.k@gmail.com>
2020-03-30 18:40:54 -0400

    Add project files.

---
+++ BandTogether.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BandTogether.MVC.Startup))]
+namespace BandTogether.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
